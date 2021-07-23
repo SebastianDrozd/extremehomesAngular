e05b094643bc6734a0f6d8a509c1794b1fb37651
Sebastian <srankoin@gmail.com>
2021-07-22 23:56:25 -0500

    done for the night. everything is setup

--- src/app/estimates/estimates-routing.module.ts
+++ src/app/estimates/estimates-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatecustomerComponent } from './createestimate/createcustomer/createcustomer.component';
+import { CreateestimateComponent } from './createestimate/createestimate.component';
+import { WorkComponent } from './createestimate/work/work.component';
 import { EstimateshomeComponent } from './estimateshome/estimateshome.component';
 
 const routes: Routes = [
-  {path: '', component: EstimateshomeComponent }
+  {path: '', component: EstimateshomeComponent },
+  {path: 'createEstimate', component:CreateestimateComponent, children:[
+    {path: '' , component: CreatecustomerComponent},
+    {path: 'work', component : WorkComponent }
+  ] }
 ];
 
 @NgModule({
--- src/app/estimates/estimates.module.ts
+++ src/app/estimates/estimates.module.ts
@@ -4,12 +4,20 @@ import { CommonModule } from '@angular/common';
 import { EstimatesRoutingModule } from './estimates-routing.module';
 import { EstimateshomeComponent } from './estimateshome/estimateshome.component';
 import { EstimatestableComponent } from './estimateshome/estimatestable/estimatestable.component';
+import { CreateestimateComponent } from './createestimate/createestimate.component';
+import { CreatecustomerComponent } from './createestimate/createcustomer/createcustomer.component';
+import { ProgressComponent } from './createestimate/progress/progress.component';
+import { WorkComponent } from './createestimate/work/work.component';
 
 
 @NgModule({
   declarations: [
     EstimateshomeComponent,
-    EstimatestableComponent
+    EstimatestableComponent,
+    CreateestimateComponent,
+    CreatecustomerComponent,
+    ProgressComponent,
+    WorkComponent
   ],
   imports: [
     CommonModule,
