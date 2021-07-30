e4fb7f7afea06335fe96995f7f14cbf4889b9f3e
Sebastian <srankoin@gmail.com>
2021-07-30 17:50:28 -0500

    additions

--- src/app/estimator/estimatorhome/shortcuts/shortcuts.component.ts
+++ src/app/estimator/estimatorhome/shortcuts/shortcuts.component.ts
@@ -1,9 +1,37 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shortcuts',
   templateUrl: './shortcuts.component.html',
-  styleUrls: ['./shortcuts.component.css']
+  styleUrls: ['./shortcuts.component.css'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({opacity: 0}),
+        animate(1000)
+      ])
+    ]),
+    trigger('fade2', [
+      transition('void => *', [
+        style({opacity: 0}),
+        animate(2000)
+      ])
+    ]),
+    trigger('fade3', [
+      transition('void => *', [
+        style({opacity: 0}),
+        animate(3000)
+      ])
+    ]),
+    trigger('fade4', [
+      transition('void => *', [
+        style({opacity: 0}),
+        animate(4000)
+      ])
+    ])
+
+  ]
 })
 export class ShortcutsComponent implements OnInit {
   @Input() customers = []
