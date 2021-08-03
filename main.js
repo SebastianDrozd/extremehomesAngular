d3cd8782c0e53ff1151b8e1ff0d1b16ed2e98b83
Sebastian <srankoin@gmail.com>
2021-08-03 17:43:53 -0500

    added pagination and more drawing functions

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
-
-
-
-
-
-
-
-
+let context ;
+let canvas;
 export default function start(url){
 
  
-const canvas = document.getElementById("canvas");
+ canvas = document.getElementById("canvas");
+ context = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 400;
 var background = new Image()
 console.log("url",url)
 background.src = url;
 
-let context = canvas.getContext("2d");
+
 background.onload = function(){
     context.drawImage(background,0,0);   
 }
@@ -46,7 +41,7 @@ function draw(event){
 	if(is_drawing){
 		context.lineTo(event.clientX - canvas.getBoundingClientRect().left, 
 			event.clientY - canvas.getBoundingClientRect().top);
-		context.strokeStyle = draw_color;
+			context.strokeStyle = "red";
 		context.lineWidth = draw_width;
 		context.lineCap = "round";
 		context.lineJoin = "round";
@@ -66,16 +61,91 @@ function stop(event){
 
 
 }
+export function start2(url){
+	
+	console.log("doing select function")
+
+	
+	let draw_color = "black";
+	let draw_width = "2";
+	let is_drawing = false;
+	// using cloneNode()
+	const sourceElement = document.querySelector('#canvas');
+	const destination = document.querySelector('#canvas');
+
+	const copy = sourceElement.cloneNode(true);
+	destination.appendChild(copy);
+	canvas = document.getElementById("canvas");
+	 context = canvas.getContext("2d");
+	 console.log("contest", context)
+	 
+	 canvas.addEventListener("touchstart", start2), false;
+
+	 canvas.addEventListener("mousedown", start2, false);
+	 
+	 
+	 canvas.addEventListener("mouseout", stop, false);
+	
+	function start2(event) {
+		console.log("start called")
+		if(is_drawing){
+			context.lineTo(event.clientX - canvas.getBoundingClientRect().left  , event.clientY - canvas.getBoundingClientRect().top);
+			context.strokeStyle = draw_color;
+			context.lineWidth = draw_width;
+			context.lineCap = "round";
+			context.lineJoin = "round";
+			context.stroke();
+		}
+		else{
+		context.beginPath();
+		console.log("path began")
+		context.moveTo(event.clientX - canvas.getBoundingClientRect().left  , event.clientY - canvas.getBoundingClientRect().top);
+		event.preventDefault();
+		is_drawing = true;
+		}
+	}
+	function draw(event){
+		if(is_drawing){
+			context.lineTo(event.clientX - canvas.offsetLeft, 
+				event.clientY - canvas.offsetTop);
+			context.strokeStyle = draw_color;
+			context.lineWidth = draw_width;
+			context.lineCap = "round";
+			context.lineJoin = "round";
+			context.stroke();
+		}
+	}
+	
+	function stop(event){
+		if(is_drawing){
+			console.log("exited")
+			context.fillStyle ='rgb(204, 255, 255)'
+			context.fill();
+			
+			context.closePath();
+			is_drawing= false;
+		}
+	}
+	
+	}
 export function download(){
 	var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"); 
 	console.log(image)
 	window.location.href=image;
 }
 export function getImage(){
+	canvas = document.getElementById("canvas");
 	var image = canvas.toDataURL("image/png")
 	//image = new Image();
     //image.src = canvas.toDataURL("image/png")
 	return image
 }
+export function getCanvas(){
+	return canvas
+}
+export function getContext(){
 
-export function setColor(){}
+	return context
+}
+
+
--- src/app/estimates/createestimate/pictures/pictures.component.ts
+++ src/app/estimates/createestimate/pictures/pictures.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import start from '../../../../../main.js'
 import * as file from '../../../../../main.js'
 import { ActivatedRoute, Router } from '@angular/router';
@@ -24,6 +24,8 @@ import { animate, style, transition, trigger } from '@angular/animations';
   ]
 })
 export class PicturesComponent implements OnInit {
+  @ViewChild('myCanvas', {static: false}) canvas: ElementRef <HTMLCanvasElement>;
+   public context: CanvasRenderingContext2D
 
   constructor( private router : Router, private route : ActivatedRoute, private estimateService : EstimatesService) { }
   wantsPictures = false;
@@ -32,7 +34,8 @@ export class PicturesComponent implements OnInit {
   picture = null
   wantsMorePictures = true;
   pictures= []
-  color = "";
+  wantSelect = false;
+  is_drawing = false;
   ngOnInit(): void {
     
   }
@@ -47,17 +50,21 @@ export class PicturesComponent implements OnInit {
 
 
   onSelectFile(event){
-    console.log(event)
-    console.log(event.target.files)
+    
     if(event.target.files){
       var reader = new FileReader();
       reader.readAsDataURL(event.target.files[0]);
       reader.onload=(e: any) => {
         this.url=e.target.result;
-        start(this.url);
-      }
+        this.drawStartMain();
+        console.log("context picture",this.context)
+        
     }
     this.uploaded=true;
+    
+    }
+    
+    
   }
 
   continue(){
@@ -70,17 +77,203 @@ export class PicturesComponent implements OnInit {
   uploadNewImage(){
     this.uploaded = false;
     this.pictures.push({imageBase: file.getImage()})
-    this.wantsMorePictures= true;
     console.log(this.pictures)
-    console.log(this.pictures.length)
+    this.wantsMorePictures= true;
+    
   }
 
   no(){
     this.router.navigate(['../summary'],  {relativeTo: this.route})
   }
 
-  setColor(){
-    start.canvas
-    console.log("color",start.draw_color)
+  turnRed(){
+    
+    this.context.strokeStyle = "red"
+  }
+  turnBlue(){
+    
+    this.context.strokeStyle = "blue"
+  }
+  turnGreen(){
+    
+    this.context.strokeStyle = "green"
+  }
+  turnYellow(){
+    
+    this.context.strokeStyle = "yellow"
+  }
+
+  wantsSelect(){
+    this.wantSelect = !this.wantSelect
+
+    
+    //@ts-ignore
+    this.canvas.removeEventListener("touchstart", this.start, false);
+    //@ts-ignore
+   this.canvas.removeEventListener("touchmove",this. draw, false);
+    //@ts-ignore
+   this.canvas.removeEventListener("mousedown", this.start, false);
+    //@ts-ignore
+   this.canvas.removeEventListener("mousemove", this.draw, false);
+    //@ts-ignore
+   this.canvas.removeEventListener("touchend", this.stop, false);
+    //@ts-ignore
+   this.canvas.removeEventListener("mouseup", this.stop, false);
+    //@ts-ignore
+   this.canvas.removeEventListener("mouseout", this.stop, false);
+    console.log(this.canvas)
+    //file.start2()
+
+   //@ts-ignore 
+  this.canvas.addEventListener("touchstart", this.startSelect, false);
+    //@ts-ignore
+  this.canvas.addEventListener("mousedown", this.startSelect, false);
+
+    //@ts-ignore
+  this.canvas.addEventListener("mouseout", this.stopSelect, false);
+
+
+
+  }
+
+  drawStartMain(){
+    //@ts-ignore
+    this.canvas = document.getElementById("canvas");
+    //@ts-ignore
+    this.context = this.canvas.getContext("2d");
+    //@ts-ignore
+   this.canvas.width = 800;
+   //@ts-ignore
+  this.canvas.height = 400;
+  let bgImg = new Image();
+  bgImg.src = this.url;
+  bgImg.onload = () => {
+      this.context.drawImage(bgImg, 0, 0);
+  }
+ //@ts-ignore
+ this.canvas.addEventListener("touchstart", this.start, false);
+  //@ts-ignore
+ this.canvas.addEventListener("touchmove",this. draw, false);
+  //@ts-ignore
+ this.canvas.addEventListener("mousedown", this.start, false);
+  //@ts-ignore
+ this.canvas.addEventListener("mousemove", this.draw, false);
+  //@ts-ignore
+ this.canvas.addEventListener("touchend", this.stop, false);
+  //@ts-ignore
+ this.canvas.addEventListener("mouseup", this.stop, false);
+  //@ts-ignore
+ this.canvas.addEventListener("mouseout", this.stop, false);
+
+  }
+   start(event) {
+    this.is_drawing = true;
+   //@ts-ignore
+   this.canvas = document.getElementById("canvas");
+   //@ts-ignore
+   this.context = this.canvas.getContext("2d");
+   console.log("started")
+   console.log("context in start", this.context)
+    this.context.beginPath();
+    //@ts-ignore
+    this.context.moveTo(event.clientX - this.canvas.getBoundingClientRect().left  , event.clientY - this.canvas.getBoundingClientRect().top);
+  
+    event.preventDefault();
+  }
+
+   draw(event){
+    if(this.is_drawing){
+      //@ts-ignore
+      this.context.lineTo(event.clientX - this.canvas.getBoundingClientRect().left, 
+      //@ts-ignore
+        event.clientY - this.canvas.getBoundingClientRect().top);
+      this.context.lineWidth = 2;
+      this.context.lineCap = "round";
+      this.context.lineJoin = "round";
+      this.context.stroke();
+    }
+  }
+  stop(event){
+    if(this.is_drawing){
+      this.context.stroke();
+      this.context.closePath();
+      this.is_drawing= false;
+      
+    }
+  
+  
+  }
+
+  startSelect(event){
+    //@ts-ignore
+    this.canvas = document.getElementById("canvas");
+    //@ts-ignore
+    this.context = this.canvas.getContext("2d");
+    console.log("started")
+        if(this.is_drawing){
+          //@ts-ignore
+          this.context.lineTo(event.clientX - this.canvas.getBoundingClientRect().left, 
+          //@ts-ignore
+            event.clientY - this.canvas.getBoundingClientRect().top);
+          this.context.strokeStyle =  "#e6fcec";
+          this.context.lineWidth = 1;
+          this.context.lineCap = "round";
+          this.context.lineJoin = "round";
+          this.context.stroke();
+        }
+        else{
+        this.context.beginPath();
+        //@ts-ignore
+        this.context.moveTo(event.clientX - this.canvas.getBoundingClientRect().left, 
+        //@ts-ignore
+          event.clientY - this.canvas.getBoundingClientRect().top);
+        event.preventDefault();
+        this.is_drawing = true;
+        }
+  }
+
+  stopSelect(){
+    if(this.is_drawing){
+			console.log("exited")
+			this.context.fillStyle ='rgb(230, 252, 236,0.5)'
+			this.context.fill();
+			
+			this.context.closePath();
+			this.is_drawing= false;
+		}
+  }
+
+  getImage2(){
+    
+    //@ts-ignore
+    var image = this.canvas.toDataURL("image/png")
+    //image = new Image();
+      //image.src = canvas.toDataURL("image/png")
+    return image
+  }
+
+  wantsDraw(){
+    this.wantSelect = false;
+    //@ts-ignore
+    this.canvas.removeEventListener("touchstart", this.startSelect, false);
+    //@ts-ignore
+    this.canvas.removeEventListener("mousedown", this.startSelect, false);
+    //@ts-ignore
+    this.canvas.removeEventListener("mouseout", this.stopSelect, false);
+
+        //@ts-ignore
+    this.canvas.addEventListener("touchstart", this.start, false);
+    //@ts-ignore
+    this.canvas.addEventListener("touchmove",this. draw, false);
+    //@ts-ignore
+    this.canvas.addEventListener("mousedown", this.start, false);
+    //@ts-ignore
+    this.canvas.addEventListener("mousemove", this.draw, false);
+    //@ts-ignore
+    this.canvas.addEventListener("touchend", this.stop, false);
+    //@ts-ignore
+    this.canvas.addEventListener("mouseup", this.stop, false);
+    //@ts-ignore
+    this.canvas.addEventListener("mouseout", this.stop, false);
   }
 }
