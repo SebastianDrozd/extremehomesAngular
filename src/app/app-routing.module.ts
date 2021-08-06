053113fb55e5e4000b137aa6dbc89e571a304bd9
Sebastian <srankoin@gmail.com>
2021-08-06 16:24:12 -0500

    netlify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', loadChildren: () => import('./estimator/estimator.module').then(m => m.EstimatorModule)},
-  {path: 'estimates', loadChildren: () => import('./estimates/estimates.module').then(m => m.EstimatesModule)},
+  {path: '/estimates', loadChildren: () => import('./estimates/estimates.module').then(m => m.EstimatesModule)},
   {path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule)}
 ];
 
